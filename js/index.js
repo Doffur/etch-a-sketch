b1928c7a23983bef3033454a0426fe461c031f63
Samir Ahajin <samirahajin@gmail.com>
2021-09-27 15:08:27 +0800

    created loop for the grid and added some css.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-let contaner = document.getElementById("container");
+let container = document.querySelector(".grid-container");
+
+
+function insertDiv(counter){
+    for(let count = 1 ; count <= counter*counter ; count++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('grid-item'+count);
+        container.appendChild(newDiv);
+
+    }
+    
+}
+
+insertDiv(16);
