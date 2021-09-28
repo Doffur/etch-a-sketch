2000987d2024a23d96a4932f107c9fe32d514884
Samir Ahajin <samirahajin@gmail.com>
2021-09-28 20:03:38 +0800

    added grid in html,js and css

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 let container = document.querySelector(".grid-container");
+let value = document.querySelector(':root');
+let items = document.querySelector('.grid-item');
+
+
 
 
 function insertDiv(counter){
+    value.style.setProperty('--val',counter);
     for(let count = 1 ; count <= counter*counter ; count++){
         let newDiv = document.createElement('div');
-        newDiv.classList.add('grid-item'+count);
+        newDiv.classList.add('grid-item');
+        newDiv.classList.add('item-'+ count);
         container.appendChild(newDiv);
-
     }
-    
+   
+
+ 
+}
+function addColor(){
+    items.classList.add('red');
 }
 
-insertDiv(16);
+insertDiv(100);
+items.addEventListener("mouseover",addColor);
